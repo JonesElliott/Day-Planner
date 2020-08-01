e820d7524f50f398d661f92c7acd1385b924ce88
Elliott Jones <jones.elliott@outlook.com>
2020-08-01 00:56:40 -0700

    Added 12 & 24 hour format toggle to updateTime function

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 // This ensures that the jQuery code does not run until the HTML has fully loaded
 $(document).ready(function () {
-    // Function to get the current time using moment()
-    function updateTime() {
-        const timeNow = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-        $('#currentDay').text(timeNow);
-    }
-    // Using setInterval() and moment() in the updateTime() function the time is being updated in real time (Hehehe)
-     setInterval(updateTime, 1000);
-     updateTime();
-
+    
 });
 
